7ebfff670d7ad3cb9b5af1348f3cbe628a0dc762
Bergeggi <152452013+Bergeggi@users.noreply.github.com>
2023-11-30 01:20:52 +0000

    entrega

---
+++ scriptzin.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE games;
+
+USE games ;
+
+CREATE TABLE plataformas ( 
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE categorias ( 
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL 
+);
+
+
+CREATE TABLE jogos ( 
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    competitivo BOOLEAN NOT NULL
+);
+
+DESC jogos;
+
+SHOW DATABASES;
